3a6c3f713c56f8236528b1fd160427e76f3489ee
Scott Sanford <ssanford@mediafly.com>
2016-11-15 15:53:32 -0600

    Add icons, switches, and more JSON!

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ angular.module("pureStorageApp", [
         'ui.bootstrap', 
         'ui.router', 
         'angAccordion', 
-        'ngMaterial'
+        'ngMaterial', 
+        'ngMdIcons'
     ])
 
-    .config(function ($compileProvider, $stateProvider, $urlRouterProvider) { 
+    .config(function ($compileProvider, $stateProvider, $urlRouterProvider, $mdIconProvider) { 
+
+          $mdIconProvider
+           .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+           .defaultIconSet('img/icons/sets/core-icons.svg', 24);
 
           $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|mfly):/);        
           $compileProvider.imgSrcSanitizationWhitelist(/^(mfly:|http:\/\/|https:\/\/)/);
--- common/js/tabs.js
+++ common/js/tabs.js
@@ -22,7 +22,8 @@ angular.module('pureStorageApp')
                   },
                   {
                     inputTitle: 'Factor in Thin Provisioning?',
-                    type: 'boolean'
+                    type: 'boolean', 
+                    value: true
                   }
                 ]
               },
@@ -46,10 +47,84 @@ angular.module('pureStorageApp')
           ]
         }, 
         {
-          title: 'Pure Configuration'
+          title: 'Pure Configuration', 
+          categories: [
+              {
+                subTitle: "Pure Configuration Inputs",
+                inputs: [
+                  {
+                    inputTitle: 'Minimum Year 1 Capacity Required (Effective TBs)',
+                    type: 'number', 
+                    value: null
+                  },
+                  {
+                    inputTitle: 'FlashArray Generation in Use for TCO',
+                    type: 'text'
+                  },
+                  {
+                    inputTitle: 'Data Reduction in Use for TCO (X:1) Given Workload Mix (without Thin Prov.)',
+                    type: 'text'
+                  },
+                  {
+                    inputTitle: '# of Arrays (Quantity)',
+                    type: 'text'
+                  }
+                ]
+              },
+              {
+                subTitle: "Pure Solution Costs",
+                inputs: [
+                  {
+                    inputTitle: 'Include Forever Flash?',
+                    type: 'boolean'
+                  },
+                  {
+                    inputTitle: 'Average UpgradeFlex CAPEX as % of Initial Purchase',
+                    type: 'number'
+                  }
+                ]
+              }
+          ]
         }, 
         {
-          title: 'Disk/Retrofit'
+          title: 'Disk/Retrofit', 
+          categories: [
+              {
+                subTitle: "Across All Workloads",
+                inputs: [
+                  {
+                    inputTitle: 'Minimum Year 1 Capacity Required (Effective TBs)',
+                    type: 'number', 
+                    value: null
+                  },
+                  {
+                    inputTitle: 'Annual Storage Expansion Growth Rate (CAGR)',
+                    type: 'text'
+                  },
+                  {
+                    inputTitle: 'Factor in Thin Provisioning?',
+                    type: 'boolean'
+                  }
+                ]
+              },
+              {
+                subTitle: "Workload Mix",
+                inputs: [
+                  {
+                    inputTitle: 'Generic VSI',
+                    type: 'number'
+                  },
+                  {
+                    inputTitle: 'SQL Server',
+                    type: 'number'
+                  },
+                  {
+                    inputTitle: 'VDI',
+                    type: 'number'
+                  }
+                ]
+              }
+          ]
         }
       ];
 
